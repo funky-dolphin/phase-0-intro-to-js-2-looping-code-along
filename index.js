83393065fad0dfcf52fe4e274b67a9053bd966f1
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-01-23 04:31:02 -0500

    commit tree c4ac52939cf3668c0c0e87a9e7f887e86ea7ddf6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, gift) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push("Thank you, " + names[i] + ", for the wonderful " + gift + " gift!");
+    }
+    return messages;
+}
+
+const count = 10
+function countDown(count){
+let countDown = 10
+while (countDown >= 0){
+    console.log(countDown--)
+}
+} countDown(10)
