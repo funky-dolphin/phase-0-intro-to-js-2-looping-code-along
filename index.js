d92e4af8f71f2f54514d646dd881020d23422a4c
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-02-05 17:23:32 -0500

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+function writeCards(array, gift){
+    let cards = []
+    for (let i = 0; i<array.length; i++){
+        cards.push(`Thank you, ${array[i]}, for the wonderful ${gift} gift!`);
+    } return cards
+} 
+function countDown(number){
+    while(number >= 0){
+        console.log(number--)
+    }
+}
